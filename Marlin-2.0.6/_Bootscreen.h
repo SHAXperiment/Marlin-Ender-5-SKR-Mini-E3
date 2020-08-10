ed8f73f6b1bc810b36e20571d0e92ec3ed8a6229
SHAXperiment <shax@shax.org>
2020-08-10 09:32:02 +0200

    Update _Bootscreen.h
    Added description to the file. Ender Bootscreen Logo.

--- Marlin-2.0.6/_Bootscreen.h
+++ Marlin-2.0.6/_Bootscreen.h
@@ -21,13 +21,7 @@
  */
 
 /**
- * Custom Boot Screen bitmap
- *
- * Place this file in the root with your configuration files
- * and enable SHOW_CUSTOM_BOOTSCREEN in Configuration.h.
- *
- * Use the Marlin Bitmap Converter to make your own:
- * https://marlinfw.org/tools/u8glib/converter.html
+ * Ender Boot Screen Logo
  */
 
 #define CUSTOM_BOOTSCREEN_TIMEOUT 1000
